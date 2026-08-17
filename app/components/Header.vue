357287d2475ef103c3e9dcc0b780535afee51791
Sam Li <weisen.li@hotmail.com>
2026-08-17 19:06:49 +0000

    remove gallery page

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -121,7 +121,6 @@ const navigationMenus = computed(() => {
     { label: '首页', to: '/' },
     { label: '校友名录', to: '/alumni' },
     { label: '校友活动', to: '/events' },
-    { label: '校友相册', to: '/gallery' },
     { label: '关于母校', to: '/about' },
     { label: '联系我们', to: '/contact' }
   ]
